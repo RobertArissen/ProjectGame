d6eeac0564d6fd32345ceb20422aaa88161805fa
Robert Arissen <info@robertarissen.nl>
2019-01-22 12:32:07 +0100

    Seeder fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -107,6 +107,135 @@ class DatabaseSeeder extends Seeder
             'updated_at' => date("Y-m-d H:i:s")
         ]);
 
+        DB::table('questionType')->insert([
+            'name' => 'Sorteervraag',
+            'inCode' => 'sort',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('questionType')->insert([
+            'name' => 'Waar / Niet waar',
+            'inCode' => 'tf',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('questionType')->insert([
+            'name' => 'Multiple choice',
+            'inCode' => 'mc',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('question')->insert([
+            'name' => 'Sorteer de volgende gebeurtenissen op volgorde in tijd van verleden naar heden',
+            'questionType_id' => 1,
+            'period_id' => 1,
+            'points' => 200,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('question')->insert([
+            'name' => 'Leven wij nu in de Middeleeuwen?',
+            'questionType_id' => 2,
+            'period_id' => 1,
+            'points' => 50,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('question')->insert([
+            'name' => 'Wanneer waren de Middeleeuwen?',
+            'questionType_id' => 3,
+            'period_id' => 1,
+            'points' => 100,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'kruistochten',
+            'correct' => 1,
+            'order' => 1,
+            'question_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'Honderdjarige Oorlog',
+            'correct' => 1,
+            'order' => 2,
+            'question_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'uitvinding van de boekdrukkunst',
+            'correct' => 1,
+            'order' => 3,
+            'question_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'Waar',
+            'correct' => 0,
+            'order' => null,
+            'question_id' => 2,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'Niet waar',
+            'correct' => 1,
+            'order' => null,
+            'question_id' => 2,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'ca. 500 tot ca. 1500 n.Chr.',
+            'correct' => 1,
+            'order' => null,
+            'question_id' => 3,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'ca. 500 voor tot ca. 500 n.Chr.',
+            'correct' => 0,
+            'order' => null,
+            'question_id' => 3,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'ca. 1500 tot ca. 1700 n.Chr.',
+            'correct' => 0,
+            'order' => null,
+            'question_id' => 3,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('answers')->insert([
+            'name' => 'ca. 1500 tot ca. 500 v.Chr.',
+            'correct' => 0,
+            'order' => null,
+            'question_id' => 3,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
         $this->call(UsersTableSeeder::class);
         $this->call(VoyagerDatabaseSeeder::class);
         $this->call(VoyagerDummyDatabaseSeeder::class);
